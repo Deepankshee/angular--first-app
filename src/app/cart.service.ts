de68154d9ad188cdd6acbb66e757845ef48d7f6c
Deepankshee <jain.deepankshee190@gmail.com>
2019-10-09 12:03:43 +0530

    first application completed

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +20,12 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
+  constructor(
+     private http: HttpClient
+  ) { }
 
 }
